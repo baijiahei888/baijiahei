e72cf8863686b61b59ff70a4609065c59cd1d073
xia <284376485@qq.com>
2017-12-13 14:58:48 +0800

    add tongji

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -139,6 +139,14 @@
     }  
   
 </script>  
-
+<script>
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?0e399e93b08c938075982aa7df01603c";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+</script>
 </body>
 </html>
